f3aab76306b24276cb1fbeaff73c1bd79e1c31a3
Marcin Januszewski <januszewskimar@gmail.com>
2020-10-31 00:25:43 +0100

    #32 Añadida la clase inicial de Cliente

---
+++ src/Cliente.ts
@@ -0,0 +1,13 @@
+class Usuario{
+	nombreUsuario: string;
+	correo: string;
+	nombre: string;
+	apellidos: string;
+	
+	constructor (nombreUsuario: string, correo: string, nombre: string, apellidos: string){
+		this.nombreUsuario = nombreUsuario;
+		this.correo = correo;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+	}
+}
