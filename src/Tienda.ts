5aa4b6c5acf10cdaf7ff45682374bc52217a85b8
Marcin Januszewski <januszewskimar@gmail.com>
2020-10-31 01:36:53 +0100

    #34 Añadidos el parámetro de opiniones y el método de añadir una nueva opinión en la clase Tienda

--- src/Tienda.ts
+++ src/Tienda.ts
@@ -1,15 +1,21 @@
 import { AdministradorTienda } from "./AdministradorTienda";
+import { Opinion } from "./Opinion";
 
 export class Tienda{
 	nombreTienda: string;
 	direccion: string;
 	telefono: string;
 	administrador: AdministradorTienda;
+	opiniones: Opinion[]
 	
 	constructor (nombreTienda: string, direccion: string, telefono: string, administrador: AdministradorTienda){
 		this.nombreTienda = nombreTienda;
 		this.direccion = direccion;
 		this.telefono = telefono;
 		this.administrador = administrador;
+	}
+	
+	aniadirOpinion(opinion: Opinion){
+		this.opiniones.push(opinion);
 	}
 }
