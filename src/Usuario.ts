eacf02787e2e8d979b6a72c43f73636e2f6c90b1
Marcin Januszewski <januszewskimar@gmail.com>
2020-11-08 22:34:04 +0100

    #32 Añadidos los métodos para la comprobación de nombre de usuario y de correo

--- src/Usuario.ts
+++ src/Usuario.ts
@@ -5,9 +5,61 @@ export class Usuario{
 	private apellidos: string;
 	
 	constructor (nombreUsuario: string, correo: string, nombre: string, apellidos: string){
-		this.nombreUsuario = nombreUsuario;
-		this.correo = correo;
+		this.setNombreUsuario(nombreUsuario);
+		this.setCorreo(correo);
 		this.nombre = nombre;
 		this.apellidos = apellidos;
 	}
+	
+	setCorreo (correo: string){
+		if (correo.length < 3){
+			throw Error("Correo incorrecto");
+		}
+		
+		let numArrobas: number = 0;
+		for (let i:number = 0 ; i < correo.length ; i++){
+			if (correo[i] == '@'){
+				numArrobas++;
+			}
+			if (correo[i] == ' '){
+				throw Error("Correo incorrecto");
+			}
+		}
+		if (numArrobas != 1 || correo[0] == '@' || correo[correo.length-1] == '@'){
+			throw Error("Correo incorrecto");
+		}
+		
+		this.correo = correo;
+	}
+	
+	setNombreUsuario (nombreUsuario: string){
+		if (nombreUsuario.length < 1){
+			throw Error("Nombre de usuario incorrecto");
+		}
+		
+		for (let i:number = 0 ; i < nombreUsuario.length ; i++){
+			if (nombreUsuario[i] == ' '){
+				throw Error("Nombre de usuario incorrecto");
+			}
+		}
+		
+		this.nombreUsuario = nombreUsuario;
+	}
+	
+	getNombreUsuario(){
+		return this.nombreUsuario;
+	}
+	
+	getCorreo(){
+		return this.correo;
+	}
+	
+	getNombre(){
+		return this.nombre;
+	}
+	
+	getApellidos(){
+		return this.apellidos;
+	}
+	
 }
