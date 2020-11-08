8bf9e11ab2748e083272fc346cd844f5692cdd62
Marcin Januszewski <januszewskimar@gmail.com>
2020-11-08 22:39:50 +0100

    #48 Añadido Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    ts: {
+      default: {
+        src: ["**/*.ts", "!node_modules/**/*.ts"]
+      },
+    },
+    mochaTest: {
+      test: {
+        src: ['src/test/*.js']
+      }
+    }
+  });
+  grunt.loadNpmTasks("grunt-ts");
+  grunt.loadNpmTasks('grunt-mocha-test');
+  
+  grunt.registerTask("default", ["ts", 'mochaTest']);
+  grunt.registerTask("test", ["ts", 'mochaTest']);
+  grunt.registerTask("compile", ["ts"]);
+};
