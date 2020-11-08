cb2fcb469eb139f109d107b8f003115f4efc774c
Marcin Januszewski <januszewskimar@gmail.com>
2020-11-08 23:29:38 +0100

    #33 Añadido el método para verificar el teléfono

--- src/Tienda.ts
+++ src/Tienda.ts
@@ -2,16 +2,16 @@ import { AdministradorTienda } from "./AdministradorTienda";
 import { Opinion } from "./Opinion";
 
 export class Tienda{
-	private nombreTienda: string;
+	private nombre: string;
 	private direccion: string;
 	private telefono: string;
 	private administrador: AdministradorTienda;
 	private opiniones: Opinion[]
 	
-	constructor (nombreTienda: string, direccion: string, telefono: string, administrador: AdministradorTienda){
-		this.nombreTienda = nombreTienda;
+	constructor (nombre: string, direccion: string, telefono: string, administrador: AdministradorTienda){
+		this.nombre = nombre;
 		this.direccion = direccion;
-		this.telefono = telefono;
+		this.setTelefono(telefono);
 		this.administrador = administrador;
 	}
 	
@@ -21,5 +21,52 @@ export class Tienda{
 	
 	getOpiniones() : Opinion[] {
 		return this.opiniones;
+	}
+	
+	setTelefono(telefono: string) {
+		let i = 0;
+		let tlf;
+		if (telefono[0] == '+'){
+			i = 1;
+			if (telefono.length < 2){
+				throw Error("Telefono incorrecto");
+			}
+			tlf = telefono.substring(1);
+		}
+		else{
+			i = 0;
+			if (telefono.length < 1){
+				throw Error("Telefono incorrecto");
+			}
+			tlf = telefono;
+		}
+		
+		let num = Number(tlf);
+		
+		if (num == NaN){
+			throw Error("Telefono incorrecto");
+		}
+		
+		if (num % 1 != 0){
+			throw Error("Telefono incorrecto");
+		}
+		
+		this.telefono = telefono;
+	}
+	
+	getNombre(){
+		return this.nombre;
+	}
+	
+	getDireccion(){
+		return this.direccion;
+	}
+	
+	getTelefono(){
+		return this.telefono;
+	}
+	
+	getAdministrador(){
+		return this.administrador;
 	}
 }
