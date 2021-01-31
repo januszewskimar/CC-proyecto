56bed3f6057d829105768131fc350512f929bdb2
Marcin Januszewski <januszewskimar@gmail.com>
2021-01-31 13:56:30 +0100

    #32 Añadido el servidor de usuarios con configuración distribuida

---
+++ src/usuarios/ServidorUsuarios.ts
@@ -0,0 +1,14 @@
+const { Etcd3 } = require('etcd3');
+const client = new Etcd3();
+import app from "./RutasUsuarios";
+
+(async() => {
+	var puerto = await client.get('puerto').string();
+
+	if (puerto == null){
+		puerto = process.env.PORT || 8080;
+	}
+	
+	app.listen(puerto);
+	console.log('Escuchando en http://127.0.0.1:' + puerto + '/');
+})()
