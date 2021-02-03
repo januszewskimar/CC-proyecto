bb083a22712a0afea049425c4a0f527a8b4d105c
Marcin Januszewski <januszewskimar@gmail.com>
2021-02-03 16:10:50 +0100

    #34 #35 #66 #67 #71 #72 #81 #82 Ajustados los test de rutas de opiniones a los cambios en el modelo

--- src/opiniones/test/RutasOpiniones.ts
+++ src/opiniones/test/RutasOpiniones.ts
@@ -5,12 +5,12 @@ import app from '../RutasOpiniones';
 var request = require('supertest');
 
 
-describe('ServidorOpiniones', function(){
+describe('Rutas de Opiniones', function(){
   describe('POST /tiendas/:tienda/opiniones', function() {
     it('Publica una opinión correcta', function(done) {
       var data = { "nombreUsuario": "david", "titulo": "Bien", "valoracionNumerica": "5", "descripcion": "Todo muy bien" }
       request(app)
-        .post('/tiendas/Tienda%20Granada/opiniones')
+        .post('/tiendas/1/opiniones')
         .send(data)
         .expect('Content-Type', /text/)
         .expect(200, done);
@@ -19,7 +19,7 @@ describe('ServidorOpiniones', function(){
     it('Devuelve error si se intenta publicar una opinión con una valoración numérica incorrecta', function(done) {
       var data = { "nombreUsuario": "david", "titulo": "Bien", "valoracionNumerica": "6", "descripcion": "Todo muy bien" }
       request(app)
-        .post('/tiendas/Tienda%20Granada/opiniones')
+        .post('/tiendas/1/opiniones')
         .send(data)
         .expect('Content-Type', /text/)
         .expect(400, done);
@@ -31,7 +31,7 @@ describe('ServidorOpiniones', function(){
   describe('GET /tiendas/:tienda/opiniones', function() {
     it('Devuelve las opiniones sobre una tienda', function(done) {
       request(app)
-        .get('/tiendas/Tienda%20Granada/opiniones')
+        .get('/tiendas/2/opiniones')
         .expect('Content-Type', /json/)
         .expect(200, done);
     });
@@ -42,7 +42,7 @@ describe('ServidorOpiniones', function(){
   describe('GET /tiendas/:tienda/valoracion-media', function() {
     it('Devuelve error si se quiere obtener la valoración media de una tienda sobre la que no hay opiniones', function(done) {
       request(app)
-        .get('/tiendas/Tienda%20XYZ/valoracion-media')
+        .get('/tiendas/3/valoracion-media')
         .expect('Content-Type', /text/)
         .expect(406, done);
     });
@@ -50,13 +50,13 @@ describe('ServidorOpiniones', function(){
     it('Devuelve la valoración media de una tienda si existen opiniones sobre ella', function(done) {
       var data = { "nombreUsuario": "carolina", "titulo": "Bien", "valoracionNumerica": "5", "descripcion": "Todo muy bien" }
       request(app)
-        .post('/tiendas/Tienda%20Albacete/opiniones')
+        .post('/tiendas/4/opiniones')
         .send(data)
         .expect('Content-Type', /text/)
         .expect(200)
         .end(function(){
           request(app)
-            .get('/tiendas/Tienda%20Albacete/valoracion-media')
+            .get('/tiendas/4/valoracion-media')
             .expect('Content-Type', /text/)
             .expect(200, done);
         });
@@ -69,13 +69,13 @@ describe('ServidorOpiniones', function(){
     it('Elimina una opinión existente', function(done) {
       var data = { "nombreUsuario": "juan", "titulo": "Regular", "valoracionNumerica": "3", "descripcion": "Regular" }
       request(app)
-        .post('/tiendas/Tienda%20ABC/opiniones')
+        .post('/tiendas/5/opiniones')
         .send(data)
         .expect('Content-Type', /text/)
         .expect(200)
         .end(function(){
           request(app)
-            .delete('/tiendas/Tienda%20ABC/opiniones/0')
+            .delete('/tiendas/5/opiniones/0')
             .expect('Content-Type', /text/)
             .expect(200, done)
         });
@@ -83,7 +83,7 @@ describe('ServidorOpiniones', function(){
 
     it('Devuelve error si se intenta eliminar una opinión inexistente', function(done) {
       request(app)
-        .delete('/tiendas/Tienda%20Granada/opiniones/241')
+        .delete('/tiendas/6/opiniones/241')
         .expect('Content-Type', /text/)
         .expect(404, done);
     });
@@ -95,7 +95,7 @@ describe('ServidorOpiniones', function(){
     it('Devuelve error si se intenta publicar una respuesta a una opinión que no existe', function(done) {
       var data = { "contenido": "No estamos de acuerdo" }
       request(app)
-        .put('/tiendas/Tienda%20RTY/opiniones/0/respuesta')
+        .put('/tiendas/7/opiniones/0/respuesta')
         .send(data)
         .expect('Content-Type', /text/)
         .expect(404, done);
@@ -105,13 +105,13 @@ describe('ServidorOpiniones', function(){
       var data1 = { "nombreUsuario": "david", "titulo": "Bien", "valoracionNumerica": "5", "descripcion": "Todo muy bien" }
       var data2 = { "contenido": "No estamos de acuerdo" }
       request(app)
-        .post('/tiendas/Tienda%20QWE/opiniones')
+        .post('/tiendas/8/opiniones')
         .send(data1)
         .expect('Content-Type', /text/)
         .expect(200)
         .end(function(){
           request(app)
-            .put('/tiendas/Tienda%20QWE/opiniones/0/respuesta')
+            .put('/tiendas/8/opiniones/0/respuesta')
             .send(data2)
             .expect('Content-Type', /text/)
             .expect(200, done);
@@ -125,7 +125,7 @@ describe('ServidorOpiniones', function(){
     it('Devuelve error si se intenta eliminar una respuesta a una opinión que no existe', function(done) {
       var data = { "contenido": "No estamos de acuerdo" }
       request(app)
-        .delete('/tiendas/Tienda%20RFL/opiniones/0/respuesta')
+        .delete('/tiendas/9/opiniones/0/respuesta')
         .send(data)
         .expect('Content-Type', /text/)
         .expect(404, done);
@@ -135,13 +135,13 @@ describe('ServidorOpiniones', function(){
       var data1 = { "nombreUsuario": "david", "titulo": "Bien", "valoracionNumerica": "5", "descripcion": "Todo muy bien" }
       var data2 = { "contenido": "No estamos de acuerdo" }
       request(app)
-        .post('/tiendas/Tienda%20UYT/opiniones')
+        .post('/tiendas/10/opiniones')
         .send(data1)
         .expect('Content-Type', /text/)
         .expect(200)
         .end(function(){
           request(app)
-            .delete('/tiendas/Tienda%20UYT/opiniones/0/respuesta')
+            .delete('/tiendas/10/opiniones/0/respuesta')
             .expect('Content-Type', /text/)
             .expect(404, done);
           });
@@ -151,19 +151,19 @@ describe('ServidorOpiniones', function(){
       var data1 = { "nombreUsuario": "david", "titulo": "Bien", "valoracionNumerica": "5", "descripcion": "Todo muy bien" }
       var data2 = { "contenido": "No estamos de acuerdo" }
       request(app)
-        .post('/tiendas/Tienda%20UYT/opiniones')
+        .post('/tiendas/11/opiniones')
         .send(data1)
         .expect('Content-Type', /text/)
         .expect(200)
         .end(function(){
           request(app)
-            .put('/tiendas/Tienda%20UYT/opiniones/0/respuesta')
+            .put('/tiendas/11/opiniones/0/respuesta')
             .send(data2)
             .expect('Content-Type', /text/)
             .expect(200)
             .end(function(){
               request(app)
-                .delete('/tiendas/Tienda%20UYT/opiniones/0/respuesta')
+                .delete('/tiendas/11/opiniones/0/respuesta')
                 .expect('Content-Type', /text/)
                 .expect(200, done);
               });
