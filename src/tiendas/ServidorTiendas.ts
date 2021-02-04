10ec74110a8a10e08caf02ee8860d279f1d173e4
Marcin Januszewski <januszewskimar@gmail.com>
2021-02-04 17:43:39 +0100

    #33 Añadidos los servidores de tiendas y de administradores de tiendas con la configuración

---
+++ src/tiendas/ServidorTiendas.ts
@@ -0,0 +1,26 @@
+const { Etcd3 } = require('etcd3');
+const client = new Etcd3();
+import winston = require('winston');
+import expressWinston = require('express-winston');
+import app from "./RutasTiendas";
+
+app.use(expressWinston.logger({
+      transports: [
+        new winston.transports.Console()
+      ],
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.json()
+      )
+}));
+
+(async() => {
+	var puerto = await client.get('puerto').string();
+
+	if (puerto == null){
+		puerto = process.env.PORT || 8080;
+	}
+	
+	app.listen(puerto);
+	console.log('Escuchando en http://127.0.0.1:' + puerto + '/');
+})()
