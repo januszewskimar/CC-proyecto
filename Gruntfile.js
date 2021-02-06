68832cd66705c542f0b8e83fbd18a67bdc3ec39a
Marcin Januszewski <januszewskimar@gmail.com>
2021-02-06 12:26:20 +0100

    Close #93 Añadidos los comandos para encender cada microservicio en la configuración del gestor de tareas

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,39 @@ module.exports = function (grunt) {
   grunt.initConfig({
     ts: {
       default: {
-        src: ["**/*.ts", "!node_modules/**/*.ts"]
+        src: ["src/**/*.ts"]
       },
     },
     mochaTest: {
       test: {
         src: ['src/**/test/*.js']
       }
+    },
+    run: {
+      usuarios: {
+        args: ['src/usuarios/ServidorUsuarios.js']
+      },
+      administradoresTiendas: {
+        args: ['src/administradores-tiendas/ServidorAdministradoresTiendas.js']
+      },
+      tiendas: {
+        args: ['src/tiendas/ServidorTiendas.js']
+      },
+      opiniones: {
+        args: ['src/opiniones/ServidorOpiniones.js']
+      }
     }
   });
   grunt.loadNpmTasks("grunt-ts");
   grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-run');
   
   grunt.registerTask("default", ["ts", 'mochaTest']);
   grunt.registerTask("test", ["ts", 'mochaTest']);
   grunt.registerTask("build", ["ts"]);
   grunt.registerTask("install", []);
+  grunt.registerTask("run-usuarios", ["ts", "run:usuarios"]);
+  grunt.registerTask("run-administadores-tiendas", ["ts", "run:administradoresTiendas"]);
+  grunt.registerTask("run-tiendas", ["ts", "run:tiendas"]);
+  grunt.registerTask("run-opiniones", ["ts", "run:opiniones"]);
 };
